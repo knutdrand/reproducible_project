67c6ea9e8120a5b10136c50583704f77d58c249e
Knut Rand <knutdrand@gmail.com>
2024-09-18 21:16:54 +0200

    test: initial test

--- tests/test_reproducible_project.py
+++ tests/test_reproducible_project.py
@@ -13,9 +13,11 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    return 'hello world'
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert response == 'hello world'
